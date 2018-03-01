9ff2f89a3f745cec42fe0e0b91747e9e983bfa89
Rizqi Fauzan <rizqiojan@Rizqiojan-3.local>
2018-03-01 23:43:26 +0100

    addReaction test

--- bot.js
+++ bot.js
@@ -179,7 +179,7 @@ bot.on('any', function(event) {
 		bot.addReaction({
 			channelID: event.d.channel_id,
 			messageID: event.d.id,
-			reaction: '💩'
+			reaction: 'MingLee'
 		});
 	}
 	if (event.t == 'MESSAGE_CREATE' && event.d.author.id == '417131923710672897' && event.d.content.includes('TATA')) {
