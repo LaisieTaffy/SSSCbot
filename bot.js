803da3b72770a569a76633fba046a2ac661ef673
Rizqi Fauzan <rizqiojan@Rizqiojan-3.local>
2018-03-02 21:58:53 +0100

    21 test

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ var auth = require('./auth.json');
 var fs = require('fs'),
 	path = require('path');
 var spawn = require('child_process').spawn;
+var deck = require('./src/deck.js');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -56,107 +57,7 @@ var aDeck,
 	botHand;
 
 /* Deck */
-//Variables
-var ranks = [ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" ];
-var suits = [ "Spade", "Heart", "Club", "Diamond" ];
 
-//Card object
-function Card(r, s) {
-	this.rank = "" + r;
-	this.suit = "" + s;
-	this.value = 0;
-}
-
-//Deck object
-function Deck(){
-	var iSuits = 0,
-		totalCards = 0;
-	var cards = [];
-	while (iSuits < 4) {
-		var iRanks = 0;
-		while (iRanks < 13) {
-			cards[totalCards] = new Card( ranks[iRanks], suits[iSuits] );
-			iRanks++;
-			totalCards++;
-		}
-		iSuits++;
-	}
-
-	//Deck of cards to call from outside
-	this.allCards = cards;
-	
-}
-
-//Shuffle
-function shuffle(deck) {
-	var i = deck.allCards.length;
-	while (--i) {
-		var j = Math.floor(Math.random() * deck.allCards.length);
-		var iTemp = deck.allCards[i];
-		var jTemp = deck.allCards[j];
-		deck.allCards[i] = jTemp;
-		deck.allCards[j] = iTemp;
-	}
-}
-
-//Draw
-function draw(deck) {
-	if (deck.allCards.length != 0) {
-		var drawn = deck.allCards.pop();
-		return drawn;
-	}
-}
-
-//Hand
-function Hand() {
-	var holeCards = [];
-	this.hand = holeCards;
-	var val = 0;
-	this.value = val;
-}
-
-//Add to hand
-function addToHand(h, card) {
-	h.hand[h.hand.length] = card;
-}
-
-//Calculate hand
-function handCalculate(h){
-	var calc = 0,
-		i = 0;
-	var tempHand = [];
-	var aceInHand = false;
-	while (i < h.hand.length) {
-		if (h.hand[i].rank == "J" || h.hand[i].rank == "Q" || h.hand[i].rank == "K") {
-			if (aceInHand) {
-				if ((calc + 10) > 21){
-					calc -= 10;
-				}
-			}
-			h.hand[i].value = 10;
-		}
-		else if (h.hand[i].rank == "A") {
-			if ((calc + 11) < 22) {
-				h.hand[i].value = 11;
-			}
-			else {
-				h.hand[i].value = 1;
-			}
-			aceInHand = true;
-		}
-		else {
-			if (aceInHand) {
-				if ((calc + h.hand[i].rank * 1) > 21){
-					calc -= 10;
-				}
-			}
-			h.hand[i].value = h.hand[i].rank;
-		}
-		calc += h.hand[i].value * 1;
-		i++;
-	}
-	return calc;
-}
 
 /* Command arguments */
 
@@ -281,14 +182,14 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     		break;
     		
     		case 'blackjack':
-    			aDeck = new Deck()
-    			shuffle(aDeck);
-    			playerHand = new Hand();
-    			botHand = new Hand();
+    			aDeck = new deck.Deck()
+    			deck.shuffle(aDeck);
+    			playerHand = new deck.Hand();
+    			botHand = new deck.Hand();
     			var x = 0;
     			while (x < 2) {
-    				addToHand(playerHand, draw(aDeck));
-    				addToHand(botHand, draw(aDeck));
+    				deck.addToHand(playerHand, deck.draw(aDeck));
+    				deck.addToHand(botHand, deck.draw(aDeck));
     				x++;
     			}
     			var botsHand = "My hand: " + botHand.hand[0].rank + " " + botHand.hand[0].suit + " and " + botHand.hand[1].rank + " " + botHand.hand[1].suit;
@@ -302,7 +203,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     		break;
     		
     		case 'hit':
-    			addToHand(playerHand, draw(aDeck));
+    			deck.addToHand(playerHand, deck.draw(aDeck));
     			var botsHand = "My hand: " + botHand.hand[0].rank + " " + botHand.hand[0].suit + " and " + botHand.hand[1].rank + " " + botHand.hand[1].suit;
     			var playersHand = "Your hand: " + playerHand.hand[0].rank + " " + playerHand.hand[0].suit;
     			var i = 1;
@@ -328,8 +229,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     			while (i < botHand.hand.length) {
     				botHand += " and " + botHand.hand[i].rank + " " + botHand.hand[i].suit;
     			}
-    			var botScore = handCalculate(botHand);
-    			var playerScore = handCalculate(playerHand);
+    			var botScore = deck.handCalculate(botHand);
+    			var playerScore = deck.handCalculate(playerHand);
     			if (botScore > playerScore){
     				bot.sendMessage({
     					to: channelID,
